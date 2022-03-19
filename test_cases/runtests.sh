40f55e9cf2067bb9915e272f86ea95411c88b4e9
jsCarleton <john.shortt@gmail.com>
2022-03-19 13:53:52 -0400

    test case renaming

--- test_cases/runtests.sh
+++ test_cases/runtests.sh
@@ -1,28 +1,17 @@
-echo "allium"
-cd allium
-time ../../wanalyze.native allium.wasm
-diff -y --suppress-common-lines *t.wat *e.wat | grep -v f64 | grep -v f32 >diff.out
-cd ../gifsicle
-echo "gifsicle"
-time ../../wanalyze.native gifsicle.wasm
-diff -y --suppress-common-lines *t.wat *e.wat | grep -v f64 | grep -v f32 >diff.out
-cd ../mozjpeg
-echo "mozjpeg"
-time ../../wanalyze.native mozjpeg.wasm
-diff -y --suppress-common-lines *t.wat *e.wat | grep -v f64 | grep -v f32 >diff.out
-cd ../oxipng
-echo "oxipng"
-time ../../wanalyze.native oxipng_bg.wasm
-diff -y --suppress-common-lines *t.wat *e.wat | grep -v f64 | grep -v f32 >diff.out
-cd ../vim-wasm
-echo "vim"
-time ../../wanalyze.native vim.wasm
-diff -y --suppress-common-lines *t.wat *e.wat | grep -v f64 | grep -v f32 >diff.out
-cd ../doom
-echo "doom"
-time ../../wanalyze.native websockets-doom.wasm
-diff -y --suppress-common-lines *t.wat *e.wat | grep -v f64 | grep -v f32 >diff.out
-echo "autocad"
-cd ../autocad
-time ../../wanalyze.native autocad.wasm
-diff -y --suppress-common-lines *t.wat *e.wat | grep -v f64 | grep -v f32 >diff.out
+#!/usr/bin/env bash
+FILELIST="allium gifsicle mozjpeg oxipng vim doom autocad"
+WATSUFFIX="-wanalyze.wat"
+
+for f in $FILELIST
+do
+    echo $f
+    cd $f
+    time ../../wanalyze.native "$f".wasm
+    diff -y --suppress-common-lines *t.wat *e.wat | grep -v f64 | grep -v f32 >diff.out
+    wc "$f""$WATSUFFIX"
+    grep "(func (" "$f""$WATSUFFIX" | wc
+    grep "loop" "$f""$WATSUFFIX" | wc
+    ls -l diff.out
+    cd ..
+    echo ""
+done
