74bbc3eb71af5867aa63d0ea83fe31467c29bbd5
jsCarleton <john.shortt@gmail.com>
2022-03-22 21:21:21 -0400

    count all paths, loop paths

--- test_cases/runtests.sh
+++ test_cases/runtests.sh
@@ -14,12 +14,16 @@ do
     np=$(grep "loop" "$f""$WATSUFFIX" | wc -l)
     lf=$(find ./funcs -name "*.wat" -print0 | xargs -0 grep -l loop | wc -l)
     nb=$(find ./funcs -name "*.paths" -size 1c | wc -l)
+    tp=$(find ./funcs -type f -name '*.paths' -exec wc -l {} \; | awk '{ total += $1 } END {print total}')
+    lp=$(grep "Loop path" funcs/*.loops | wc -l)
     # Print the result
     echo "lines of code: $nl"
     echo "# of functions: $nf"
     echo "# of loops: $np"
     echo "# functions with loops: $lf"
     echo "# functions with too many paths: $nb"
+    echo "# paths through all functions: $tp"
+    echo "# paths through all loops: $lp"
     # Compare the source file we created to the one created by wabt
     diff -y --suppress-common-lines *t.wat *e.wat | grep -v f64 | grep -v f32 >diff.out
     diffsize=$(find diff.out -printf "%s")
