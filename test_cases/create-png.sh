6e3771b0035a28c000ef97fddf5b3229a7dd8da3
jshortt <john.shortt@tradingcentral.com>
2022-04-21 10:59:56 -0400

    ran mozjpeg tests

--- test_cases/create-png.sh
+++ test_cases/create-png.sh
@@ -1,11 +1,11 @@
 for FILE in "$1"/funcs/*.bblocks; do
     nbb=$(wc -l <"$FILE")
-    if [ $nbb -gt 515 ]
+    if [ $nbb -gt 406 ]
     then
         echo "file too large $FILE"
     else
         echo "creating ${FILE%%.*}.png"
-        dot "${FILE%%.*}.dot" -Tpng -o "${FILE%%.*}.png"; 
-        dot "${FILE%%.*}-e.dot" -Tpng -o "${FILE%%.*}-e.png"; 
+        gtimeout 20s dot "${FILE%%.*}.dot" -Tpng -o "${FILE%%.*}.png"; 
+        gtimeout 20s dot "${FILE%%.*}-e.dot" -Tpng -o "${FILE%%.*}-e.png"; 
     fi
 done
