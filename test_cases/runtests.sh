3979ecda40753d057012e66fdf56495b212c50b8
jsCarleton <john.shortt@gmail.com>
2022-04-21 16:17:24 -0400

    updated runtests script

--- test_cases/runtests.sh
+++ test_cases/runtests.sh
@@ -7,16 +7,14 @@ do
     echo $f
     cd $f
     # Run our analysis
-    /usr/bin/time ../../wanalyze.native "$f".wasm
+    /usr/bin/time -p ../../wanalyze.native "$f".wasm
     # Analyze the outputs
     nl=$(wc -l <"$f""$WATSUFFIX")
     nf=$(grep "^  (func (;" "$f""$WATSUFFIX" | wc -l)
     nc=$(grep -v ' -1' "$f".costs | wc -l)
     np=$(grep "loop" "$f""$WATSUFFIX" | wc -l)
     lf=$(find ./funcs -name "*.wat" -print0 | xargs -0 grep -l loop | wc -l)
-    nb=$(find ./funcs -name "*.paths" -size 3c | wc -l)
-    tp=$(find ./funcs -type f -name '*.paths' -exec wc -l {} \; | awk '{ total += $1 } END {print total}')
-    lp=$(grep "Loop path" funcs/*.loops | wc -l)
+    nb=$(ls $(grep -l 'unknown number' funcs/*.ebblocks)|wc -l)
     # Print the result
     echo "lines of code: $nl"
     echo "# of functions: $nf"
@@ -24,11 +22,9 @@ do
     echo "# of loops: $np"
     echo "# functions with loops: $lf"
     echo "# functions with too many paths: $nb"
-    echo "# paths through all functions: $tp"
-    echo "# paths through all loops: $lp"
     # Compare the source file we created to the one created by wabt
     diff -y --suppress-common-lines *t.wat *e.wat | grep -v f64 | grep -v f32 >diff.out
-    ds=$(stat -f %z diff.out)
+    ds=$(stat --format=%s diff.out)
     if [ $ds -gt 0 ]
     then
         echo "Check diff output"
