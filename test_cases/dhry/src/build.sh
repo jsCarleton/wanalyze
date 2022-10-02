9e995e057b2c0ac28e9df81a38b5f8294389a44e
jsCarleton <john.shortt@gmail.com>
2022-10-02 11:34:30 -0400

    gas calculations

--- test_cases/dhry/src/build.sh
+++ test_cases/dhry/src/build.sh
@@ -1,2 +1,2 @@
-emcc --emit-symbol-map -O2 -sEXPORTED_FUNCTIONS=_main cpuidc64.c dhry_1.c dhry_2.c -o dhry.js
-gcc -O2 cpuidc64.c dhry_1.c dhry_2.c -o dhry
+emcc -O3 -sEXPORTED_FUNCTIONS=_main -s STANDALONE_WASM cpuidc64.c dhry_1.c dhry_2.c -o dhry.js
+gcc -O3 cpuidc64.c dhry_1.c dhry_2.c -o dhry
