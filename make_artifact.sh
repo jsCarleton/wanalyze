22c400651f1c02f5ab3f87091ba9bfbe1e040acb
jshortt <60323583+jsCarleton@users.noreply.github.com>
2023-12-13 15:46:10 -0500

    preparing artifact

---
+++ make_artifact.sh
@@ -0,0 +1,3 @@
+zip wanalyze.zip -r LICENSE README.MD *.sh design_notes src *.sh test_cases/*/*.wasm test_cases/*/*.c test_cases/*.sh \
+    test_cases/dhry/src test_cases/ebpf/src test_cases/musl
+cp wanalyze.zip /users/johnshortt/"VirtualBox Shared"
